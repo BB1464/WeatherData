33e36f76a60d1b0143f71ea3243d0210be9ae90d
BB1464 <oluwafemioyedele@gmail.com>
2023-07-02 17:10:05 +0100

    Change the climate service to make the download faster

--- script/downloadingScript.R
+++ script/downloadingScript.R
@@ -11,9 +11,9 @@ dates <- c("2020-01-01","2022-12-31")
 lonlat <- data.frame(lon = c(7.01656), lat = c(4.517005))
 
 # The get_chirps function retrieve all the rainfall data
-r1 <- get_chirps(lonlat, dates, server = "CHC")
+r1 <- get_chirps(lonlat, dates, server = "ClimateSERV")
 
 # The get_chirts function retreive the temperature(max and min), and relative humidity data
 
-get_chirts(lonlat, dates, server = "CHC")
+get_chirts(lonlat, dates, server = "ClimateSERV")
 
