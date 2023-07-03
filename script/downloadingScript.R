b36ab7c0c5ffe0b80a5d43cbc2440cf962185d0b
BB1464 <oluwafemioyedele908@gmail.com>
2023-07-03 07:52:35 +0100

    Update my file

--- script/downloadingScript.R
+++ script/downloadingScript.R
@@ -3,6 +3,8 @@
 
 library("chirps")
 library("terra")
+library(writexl)
+library(tidyverse)
 
 # Case 1: return as a data.frame
 # Dates to Retrieve the data from
@@ -15,7 +17,7 @@ r1 <- get_chirps(lonlat, dates, server = "ClimateSERV")
 
 # The get_chirts function retreive the temperature(max and min), and relative humidity data
 
-Tmax <- get_chirts(lonlat, dates, server = "ClimateSERV",var = 'Tmax')
+Tmax <- get_chirts(lonlat, dates,var = 'Tmax',server = "ClimateSERV")
 Tmin <- get_chirts(lonlat, dates, server = "ClimateSERV",var = 'Tmin')
 RHum <- get_chirts(lonlat, dates, server = "ClimateSERV",var = 'RHum')
 
