1af9f5ad753891528d43981064e2176f991100c6
BB1464 <oluwafemioyedele908@gmail.com>
2023-07-03 12:10:33 +0100

    Add file

--- script/downloadingScript.R
+++ script/downloadingScript.R
@@ -8,14 +8,19 @@ library(tidyverse)
 
 # Case 1: return as a data.frame
 # Dates to Retrieve the data from
+
+# Rainfall Data
 dates <- c("2020-01-01","2022-12-31")
 
 lonlat <- data.frame(lon = c(7.01656), lat = c(4.517005))
 
 # The get_chirps function retrieve all the rainfall data but the documentation specified that the data is available from 1983 to 2016
-r1 <- get_chirps(lonlat, dates, server = "ClimateSERV")
+RF <- get_chirps(lonlat, dates, server = "ClimateSERV")
 
 # The get_chirts function retreive the temperature(max and min), and relative humidity data
+
+# Temperature and Relative Humidity
+dates <- c("2010-01-01","2016-12-31")
 
 Tmax <- get_chirts(lonlat, dates,var = 'Tmax',server = "ClimateSERV")
 Tmin <- get_chirts(lonlat, dates, server = "ClimateSERV",var = 'Tmin')
